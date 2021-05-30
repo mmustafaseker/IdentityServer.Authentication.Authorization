e9536d693edf9130bbf72b57fd8c4cfee25c98cb
mmustafaseker <mustiseker.06@gmail.com>
2021-05-30 17:37:55 +0300

    client1 - api1 bağlantısı

---
+++ IdentityServer.Client1/Controllers/ProductsController.cs
@@ -0,0 +1,64 @@
+﻿using IdentityModel.Client;
+using IdentityServer.Client1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Client1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public ProductsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async  Task<IActionResult> Index()
+        {
+            HttpClient httpClient = new HttpClient();
+            List<Product> products = new List<Product>();
+            var disco = await httpClient.GetDiscoveryDocumentAsync("https://localhost:5001");
+
+            if (disco.IsError)
+            {
+                //loglama
+            }
+
+            ClientCredentialsTokenRequest clientCredentialsTokenRequest = new ClientCredentialsTokenRequest();
+            clientCredentialsTokenRequest.ClientId = _configuration["Client:ClientId"];
+            clientCredentialsTokenRequest.ClientSecret = _configuration["Client:ClientSecret"];
+            clientCredentialsTokenRequest.Address = disco.TokenEndpoint;
+
+            var token = await httpClient.RequestClientCredentialsTokenAsync(clientCredentialsTokenRequest);
+            
+            if (token.IsError)
+            {
+                //loglama
+            }
+            //https://localhost:5006
+
+            httpClient.SetBearerToken(token.AccessToken);
+
+            var response = await httpClient.GetAsync("https://localhost:5016/api/products/getproducts");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+
+                products = JsonConvert.DeserializeObject<List<Product>>(content);
+            }
+            else
+            {
+                //loglama
+            }
+
+            return View(products);
+        }
+    }
+}
