abf0560b14340cc3878e6b9f9f1ad49c250aafa5
mmustafaseker <mustiseker.06@gmail.com>
2021-05-30 18:27:34 +0300

    UI Katmanı eklendi

--- IdentityServer.AuthServer/Config.cs
+++ IdentityServer.AuthServer/Config.cs
@@ -1,7 +1,9 @@
 ﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace IdentityServer.AuthServer
@@ -57,6 +59,45 @@ namespace IdentityServer.AuthServer
                     ClientSecrets = new[]{new Secret("secret".Sha256())},
                     AllowedGrantTypes =GrantTypes.ClientCredentials,
                     AllowedScopes ={"api1.read", "api1.update", "api2.write", "api2.update" }
+                }
+            };
+        }
+
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>()
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        public static IEnumerable<TestUser> GetUsers()
+        {
+            return new List<TestUser>()
+            {
+                new TestUser
+                {
+                    SubjectId="1",
+                    Username="Mseker",
+                    Password="password",
+                    Claims=new List<Claim>()
+                    {
+                        new Claim("given_name","Mustafa"),
+                         new Claim("family_name","Şeker")
+                    }
+                },
+                 new TestUser
+                {
+                    SubjectId="2",
+                    Username="Zyılmaz",
+                    Password="password",
+                    Claims=new List<Claim>()
+                    {
+                        new Claim("given_name","Züleyha"),
+                         new Claim("family_name","Yılmaz")
+                    }
                 }
             };
         }
